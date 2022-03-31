28dad6544ccb1719d3dc56adcc35d5ee388ea3fb
balu_balakrishnan <balu.b@factweavers.com>
2022-03-31 14:54:56 +0530

    fix: access_token issue resolved, save_customer_details() mapped to activateSubscription API

--- src/handlers/user.py
+++ src/handlers/user.py
@@ -3,30 +3,22 @@ from src.constants import *
 from datetime import datetime
 from src.services.insertUserDetails import add_user
 from dateutil.relativedelta import relativedelta
-
 import hashlib
 
-from src.utilities.utils import activation_sms_message_format, send_sms_message, get_activation_code, get_zimperium_code
 
-
-def save_customer_details():
+def save_customer_details(first_name, last_name, phone_number, email, payment_status, subscription_plan, group_id,
+                          activation_id, short_token):
     try:
-        first_name = request.json[USER_FIRST_NAME]
-        last_name = request.json[USER_LAST_NAME]
-        phone_number = request.json[USER_PHONENO]
-        email = request.json[USER_EMAIL]
-        duration = request.json[DURATION]
-        payment_status = request.json[PAYMENT_STATUS]
-        subscription_plan = request.json[USER_SUBPLAN]
-        hash_value = hashlib.md5(phone_number.encode('utf-8')).hexdigest()
+        hash_value = hashlib.md5(phone_number.encode(UTF8)).hexdigest()
         subscription_date = datetime.utcnow()
-        expiration_date = None
-        is_subscribed = False
-        is_payment_completed = False
+        expiration_date = NONE
+        is_subscribed = STATUS_FALSE
+        is_payment_completed = STATUS_FALSE
         if payment_status == PAYMENT_SUCCESS:
-            expiration_date = subscription_date + relativedelta(months=int(duration))
-            is_subscribed = True
-            is_payment_completed = True
+            expiration_date = subscription_date + relativedelta(months=int(subscription_plan))
+            is_subscribed = STATUS_TRUE
+            is_payment_completed = STATUS_TRUE
+
         data = {
             'hash_value': hash_value,
             'first_name': first_name,
@@ -37,17 +29,13 @@ def save_customer_details():
             'subscription_date': subscription_date,
             'expiration_date': expiration_date,
             'is_subscribed': is_subscribed,
-            'is_payment_completed': is_payment_completed
+            'is_payment_completed': is_payment_completed,
+            'group_id': group_id,
+            'activation_id': activation_id,
+            'short_token': short_token
         }
 
         db_response = add_user(data)
-        context = {'id': hash_value, 'first_name': first_name, 'last_name': last_name, 'phone_number': phone_number,
-                   'email': email, 'subscription_plan': subscription_plan, 'payment_status': payment_status}
-        if db_response and is_payment_completed:
-            code = get_activation_code()
-            if code:
-                message = activation_sms_message_format(first_name, code)
-                msg_status = send_sms_message(phone_number, message)
-        return jsonify(context)
+        return db_response
     except Exception as e:
         print(e.__str__())
--- src/handlers/zimperium_handler.py
+++ src/handlers/zimperium_handler.py
@@ -1,34 +1,36 @@
-import hashlib
 import json
-from datetime import datetime
 import requests
-from dateutil.relativedelta import relativedelta
 from flask import jsonify
-import jwt, time
-
+import jwt
+import time
 from config import ZIMPERIUM_LOGIN_PAYLOAD
 from src import ZIMPERIUM_HOST, ZIMPERIUM_LOGIN_API, db, ZIMPERIUM_GROUP_API, BEARER, \
     LOGIN_HEADER, AUTHORIZATION, CONTENT_TYPE, APPLICATION, STATUS_FALSE, ZIMPERIUM_ACTIVATION_API, \
-    ZIMPERIUM_ACTIVATION_LIMIT, STATUS_TRUE, PLAN_VALUE
+    ZIMPERIUM_ACTIVATION_LIMIT, PLAN_VALUE, DECODED_INITIAL, ACCESS_TOKEN, NONE, VERIFY_SIGNATURE, \
+    TOKEN_EXPIRY, UTF8, ZIMPERIUM_ACCESS_TOKEN, ZIMPERIUM_REFRESH_TOKEN, RESP_STATUS, SUB_ID, MESSAGE, GRP_ID, \
+    SHORT_TOKEN, VALUE_ZERO, PAYMENT_SUCCESS, MESSAGE_STATUS, PAYMENT_FAILED
+from src.handlers.user import save_customer_details
 from src.models.token_details import Tokens
-from src.services.insertUserDetails import add_user
-from src.utilities.utils import get_zimperium_code, activation_sms_message_format, send_sms_message, get_user_details
+from src.utilities.utils import activation_sms_message_format, send_sms_message, get_user_details
 
 
 def zimperium_login():
 
     url = f'{ZIMPERIUM_HOST}{ZIMPERIUM_LOGIN_API}'
     obj = Tokens.query.order_by(-Tokens.id).first()
-    token = obj.access_token
-    decoded = jwt.decode(token, options={"verify_signature": STATUS_FALSE})
-
-    if decoded['exp'] < time.time():
+    decoded = DECODED_INITIAL
+    token = getattr(obj, ACCESS_TOKEN, NONE)
+    if token is not NONE:
+        decoded = jwt.decode(token, options={VERIFY_SIGNATURE: STATUS_FALSE})
+    else:
+        decoded[TOKEN_EXPIRY] = VALUE_ZERO
+    if decoded[TOKEN_EXPIRY] < time.time():
         response = requests.post(url, json=ZIMPERIUM_LOGIN_PAYLOAD, headers=LOGIN_HEADER)
-        response = json.loads(response.content.decode("utf-8"))
+        response = json.loads(response.content.decode(UTF8))
 
         if response:
-            access_token = response['accessToken']
-            refresh_token = response['refreshToken']
+            access_token = response[ZIMPERIUM_ACCESS_TOKEN]
+            refresh_token = response[ZIMPERIUM_REFRESH_TOKEN]
             transaction = Tokens(access_token=access_token, refresh_token=refresh_token)
             db.session.add(transaction)
             db.session.commit()
@@ -46,10 +48,10 @@ def get_default_group_id():
         AUTHORIZATION: access_token
     }
     response = requests.get(url, headers=headers)
-    response = json.loads(response.content.decode("utf-8"))
+    response = json.loads(response.content.decode(UTF8))
     if not response:
-        return jsonify({"group_id_status": STATUS_FALSE})
-    group_id = response[0]['id']
+        return jsonify({RESP_STATUS: STATUS_FALSE})
+    group_id = response[0][SUB_ID]
     return group_id, access_token
 
 
@@ -66,7 +68,6 @@ def activate_zimperium_user():
         "groupId": group_id
     }
     payload = json.dumps(payload)
-
     url = f'{ZIMPERIUM_HOST}{ZIMPERIUM_ACTIVATION_API}'
     headers = {
         CONTENT_TYPE: APPLICATION,
@@ -74,42 +75,22 @@ def activate_zimperium_user():
     }
 
     response = requests.post(url, data=payload, headers=headers)
-    response = json.loads(response.content.decode("utf-8"))
-    if response.get('message', None):
-        return response['message']
-    subscription_plan = PLAN_VALUE
-    hash_value = hashlib.md5(mobile_no.encode('utf-8')).hexdigest()
-    subscription_date = datetime.utcnow()
-    expiration_date = subscription_date + relativedelta(months=int(subscription_plan))
-    is_subscribed = STATUS_TRUE
-    is_payment_completed = STATUS_TRUE
-    group_id = response['groupId']
-    activation_id = response['id']
-    short_token = response['shortToken']
+    response = json.loads(response.content.decode(UTF8))
 
-    data = {
-        'hash_value': hash_value,
-        'first_name': first_name,
-        'last_name': last_name,
-        'phone_number': mobile_no,
-        'email': email,
-        'subscription_plan': subscription_plan,
-        'subscription_date': subscription_date,
-        'expiration_date': expiration_date,
-        'is_subscribed': is_subscribed,
-        'is_payment_completed': is_payment_completed,
-        'group_id': group_id,
-        'activation_id': activation_id,
-        'short_token': short_token
-    }
-    db_response = add_user(data)
-    context = {'id': hash_value, 'first_name': first_name, 'last_name': last_name, 'phone_number': mobile_no,
-               'email': email, 'subscription_plan': subscription_plan, 'payment_status': is_payment_completed}
-    if db_response and is_payment_completed:
-        code = get_zimperium_code(hash_value)
-        if code:
-            message = activation_sms_message_format(first_name, code)
+    if response.get(MESSAGE, NONE):
+        return response[MESSAGE]
+    subscription_plan = PLAN_VALUE
+    group_id = response[GRP_ID]
+    activation_id = response[SUB_ID]
+    short_token = response[SHORT_TOKEN]
+    payment_status = PAYMENT_SUCCESS
+
+    db_response = save_customer_details(first_name, last_name, mobile_no, email, payment_status, subscription_plan,
+                                        group_id, activation_id, short_token)
+    if db_response:
+        if short_token:
+            message = activation_sms_message_format(first_name, short_token)
             msg_status = send_sms_message(mobile_no, message)
-    return jsonify(context)
+    return jsonify({MESSAGE_STATUS: PAYMENT_SUCCESS})
 
 
