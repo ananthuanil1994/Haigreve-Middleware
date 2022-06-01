d542b6b7d20ec3944e3afc0030372881ba1f75e0
balu_balakrishnan <balu.b@factweavers.com>
2022-06-01 12:21:43 +0530

    fixed report by adding new fields

--- src/handlers/report_generator.py
+++ src/handlers/report_generator.py
@@ -3,7 +3,7 @@ from io import StringIO
 
 from flask import request
 from src import DATE, TIME, TYPE, CSV_MOBILE_NUMBER, CSV_SUBSCRIBED, CSV_PAYMENT_COMPLETED, TEXT_CSV, CSV_FILENAME, \
-    CONTENT_DISPOSITION, CSV_ATTACHMENT
+    CONTENT_DISPOSITION, CSV_ATTACHMENT, CSV_START_DATE, CSV_END_DATE, CSV_ACTIVATION_ID
 from werkzeug import Response
 
 from src.models.transaction_details import Transactions
@@ -14,17 +14,20 @@ def report_generator():
     date = request.json[DATE]
     user_data = Transactions.query.filter(Transactions.date_updated == date). \
         join(Users, Users.mobile_number == Transactions.mobile_number).with_entities(Transactions.mobile_number,
-                                                                                     Transactions.time,
                                                                                      Transactions.type,
                                                                                      Users.is_subscribed,
-                                                                                     Users.is_payment_completed)
+                                                                                     Users.is_payment_completed,
+                                                                                     Users.subscription_date,
+                                                                                     Users.expiration_date,
+                                                                                     Users.activation_id)
 
     def generate():
         data = StringIO()
         w = csv.writer(data)
 
         # write header
-        w.writerow((CSV_MOBILE_NUMBER, TIME, TYPE, CSV_SUBSCRIBED, CSV_PAYMENT_COMPLETED))
+        w.writerow((CSV_MOBILE_NUMBER, TYPE, CSV_SUBSCRIBED, CSV_PAYMENT_COMPLETED, CSV_START_DATE, CSV_END_DATE,
+                    CSV_ACTIVATION_ID))
         yield data.getvalue()
         data.seek(0)
         data.truncate(0)
@@ -33,10 +36,12 @@ def report_generator():
         for user in user_data:
             w.writerow((
                 user.mobile_number,
-                user.time,
                 user.type,
                 user.is_subscribed,
-                user.is_payment_completed
+                user.is_payment_completed,
+                user.subscription_date,
+                user.expiration_date,
+                user.activation_id
             ))
             yield data.getvalue()
             data.seek(0)
